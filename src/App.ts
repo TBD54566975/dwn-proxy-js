da96ff408ad5314061d2e783d62458dd440debe2
Kendall Weihe <kendallw@squareup.com>
2023-06-06 10:32:20 -0400

    trying to integrate with the dids package

--- src/App.ts
+++ src/App.ts
@@ -1,3 +1,4 @@
+import { DidIonApi, DidState } from '@tbd54566975/dids';
 import { Inbound } from './Inbound.js';
 import { Outbound } from './Outbound.js';
 
@@ -6,19 +7,38 @@ import { Outbound } from './Outbound.js';
 export class App {
   inbound: Inbound;
   outbound: Outbound;
+
+  #didState: DidState;
   // middlewares: Array<IMiddleware>;
 
-  constructor() {
+  constructor () {
     this.inbound = new Inbound();
     this.outbound = new Outbound();
+
+    /**
+     * - ??? start a DWN??? is that even necessary in this exact moment?
+     */
   }
 
   // use = (middleware: IMiddleware) => {
   //   this.middlewares.push(middleware);
   // };
 
-  listen = (inboundPort: number, outboundPort: number) => {
-    console.log(inboundPort, outboundPort);
+  listen = async (inboundPort: number, outboundPort: number) => {
+    this.#didState = await new DidIonApi().create({
+      services: [{
+        id              : '#dwn',
+        type            : 'DecentralizedWebNode',
+        serviceEndpoint : {
+          nodes: [ `http://localhost:${inboundPort}` ]
+        }
+      }]
+    });
+
     this.inbound.listen(inboundPort);
+
+    console.log(`Listening to inbound on ${inboundPort}`);
+    console.log(`Listening to outbound on ${outboundPort}`);
+    console.log(this.#didState);
   };
 }
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ export { App } from './App.js';
 
 import { App } from './App.js';
 const app = new App();
+app.inbound.protocol.route('tbdex', 'offer', '/some/api/offer');
 const INBOUND_PORT = 3000;
 const OUTBOUND_PORT = 3001;
 app.listen(INBOUND_PORT, OUTBOUND_PORT);

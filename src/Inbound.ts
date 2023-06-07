c8d52566ed915e52ed5ba24119dce06fd5c563e7
Kendall Weihe <kendallw@squareup.com>
2023-06-07 09:56:49 -0400

    reverted back to match-based interface, basic handler logic

--- src/Inbound.ts
+++ src/Inbound.ts
@@ -5,49 +5,82 @@ import {
   readOctetStream } from './Http.js';
 import { parseDwm } from './JsonRpc.js';
 
-export type ProtocolRoute = {
-  protocol: string;
-  schema: string;
+export interface IHandlerHttp {
   path: string;
+  // TODO other things
 }
 
 export interface IMiddleware {
-  (message: DwnMessage): void;
+  (message: DwnMessage): Promise<void>;
 }
 
-export type ProtocolMiddleware = {
-  protocol: string;
-  schema: string;
-  middleware: IMiddleware;
+export interface IHandlerOptions {
+  http?: IHandlerHttp;
+  middleware?: IMiddleware;
+}
+
+export interface IMatchFunc {
+  (msg: DwnMessage): boolean;
+}
+
+interface IHandlerFunc {
+  (match: IMatchFunc, a: IHandlerOptions | string, middleware?: IMiddleware): void
+}
+interface IRecords {
+  write: IHandlerFunc;
+}
+
+export interface IHandler {
+  match: IMatchFunc;
+  options: IHandlerOptions;
 }
 
 export class Inbound {
-  #protocols: Array<ProtocolRoute | ProtocolMiddleware> = [];
+  #handlers: Array<IHandler> = [];
 
-  protocol = {
-    route: (protocol: string, schema: string, path: string) =>
-      this.#protocols.push({ protocol, schema, path }),
-    middleware: (protocol: string, schema: string, middleware: IMiddleware) =>
-      this.#protocols.push({ protocol, schema, middleware })
+  records: IRecords = {
+    write: (match, a, middleware) => {
+      let options: IHandlerOptions;
+      let path: string;
+
+      if (typeof a === 'string') {
+        path = a;
+      } else {
+        options = a;
+      }
+
+      let handler: IHandler = {
+        match,
+        options: options ?? {}
+      };
+      if (path) handler.options.http = { path };
+      if (middleware) handler.options.middleware = middleware;
+
+      this.#handlers.push(handler);
+    }
   };
 
   #handler: IHttpHandler = async (req, res) => {
-    const message = parseDwm(req.headers['dwn-request'] as string);
+    let message = parseDwm(req.headers['dwn-request'] as string);
+    console.log(message);
+    message = { ...message, descriptor: { ...message.descriptor, protocol: 'tbdex', schema: 'rfq' } };
     console.log(message);
 
     const data = await readOctetStream(req);
     console.log(data);
 
-    res.statusCode = 202;
+    const handler = this.#handlers.find(({ match }) => match(message));
+    console.log(handler);
+    if (!handler) {
+      res.statusCode = 404;
+    } else {
+      res.statusCode = 202;
+      if (handler.options.middleware) handler.options.middleware(message);
+      console.log(handler.options.http);
+      // forward http
+    }
+
     res.end();
-    /**
-     * - parse DWM from req
-     * - iterate handlers to find match
-     * - if no match, then respond 404
-     * - else respond 202
-     * - if middleware, then call
-     * - make downstream http req
-     */
   };
 
   // listen = async (port: number) => await this.#server.listen(port, this.#handler);
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 export { App } from './App.js';
 
 import { App } from './App.js';
+import { IMatchFunc } from './Inbound.js';
+
 const app = new App();
-app.inbound.protocol.route('tbdex', 'offer', '/some/api/offer');
+
+const isRfq: IMatchFunc = msg =>
+  msg.descriptor.protocol === 'tbdex' && msg.descriptor.schema === 'rfq';
+app.inbound.records.write(isRfq, '/some/api/rfq');
+
 const INBOUND_PORT = 3000;
 const OUTBOUND_PORT = 3001;
 app.listen(INBOUND_PORT, OUTBOUND_PORT);

a2d48adb61d92e5a0f7741fc99bbb2dcbb1545cb
Kendall Weihe <kendallw@squareup.com>
2023-06-09 09:50:30 -0400

    earmark

--- src/Outbound.ts
+++ src/Outbound.ts
@@ -1,10 +1,6 @@
 import http from 'http';
 import { DwnMessage } from './types.js';
 
-// export interface IOutboundMiddleware {
-//   (req: http.IncomingMessage, res: http.OutgoingMessage, next: NextFunction): void;
-// }
-
 export interface IOutboundRestfulMiddleware {
   (req: http.IncomingMessage, res: http.OutgoingMessage): DwnMessage;
 }
@@ -14,23 +10,7 @@ export interface IOutboundMethodMiddleware {
     middleware?: ((req: http.IncomingMessage, res: http.OutgoingMessage) => DwnMessage)): void
 }
 
-// export interface IOutboundRoute {
-//   match: (req: http.IncomingMessage) => boolean;
-//   use: IOutboundRestfulMiddleware;
-// }
-
 export class Outbound {
-  // routes: Array<IOutboundRoute>;
-  // middlewares: Array<IOutboundMiddleware>;
-
-  // use = (middleware: IOutboundMiddleware) => {
-  //   this.middlewares.push(middleware);
-  // };
-
-  // get: IOutboundMethodMiddleware = (path, middleware) => {
-  //   console.log(path, middleware);
-  // };
-
   post: IOutboundMethodMiddleware = (path, middleware) => {
     console.log(path, middleware);
   };

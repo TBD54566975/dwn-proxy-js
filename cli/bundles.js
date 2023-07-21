64925f03ad19ae33a7d2d7fe1848caa47507fc24
Kendall Weihe <kendallw@squareup.com>
2023-07-21 09:45:30 -0400

    init dedicated cli project

---
+++ cli/bundles.js
@@ -0,0 +1,25 @@
+import esbuild from 'esbuild'
+
+// cjs bundle. external dependencies **not** bundled
+esbuild.buildSync({
+  platform       : 'node',
+  bundle         : true,
+  format         : 'cjs',
+  packages       : 'external',
+  sourcemap      : true,
+  entryPoints    : ['./src/main.ts'],
+  outfile        : './dist/cjs/main.cjs',
+  allowOverwrite : true,
+})
+
+// esm bundle. external dependencies **not** bundled
+esbuild.buildSync({
+  platform       : 'node',
+  bundle         : true,
+  format         : 'esm',
+  packages       : 'external',
+  sourcemap      : true,
+  entryPoints    : ['./src/main.ts'],
+  outfile        : './dist/esm/main.mjs',
+  allowOverwrite : true,
+})
